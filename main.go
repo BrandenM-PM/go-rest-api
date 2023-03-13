7acc72605b332e4164a478a0fab5b8714a49b21c
Branden Morin <brandenmorin14@gmail.com>
2023-03-12 21:22:54 -0400

    Updates..

--- main.go
+++ main.go
@@ -10,59 +10,62 @@
 package main
 
 import (
-	"fmt"
-	"github.com/BrandenM-PM/go-rest-api/controllers"
-	_ "github.com/BrandenM-PM/go-rest-api/docs"
-	"github.com/BrandenM-PM/go-rest-api/initializers"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/swagger"
-	"github.com/gofiber/template/handlebars"
-	"log"
-	"os"
-	"strings"
-	"time"
+    "fmt"
+    "github.com/BrandenM-PM/go-rest-api/controllers"
+    _ "github.com/BrandenM-PM/go-rest-api/docs"
+    "github.com/BrandenM-PM/go-rest-api/initializers"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+    "github.com/gofiber/fiber/v2/middleware/limiter"
+    "github.com/gofiber/fiber/v2/middleware/logger"
+    "github.com/gofiber/fiber/v2/middleware/recover"
+    "github.com/gofiber/swagger"
+    "github.com/gofiber/template/handlebars"
+    "log"
+    "os"
+    "strings"
+    "time"
 )
 
 func init() {
-	initializers.LoadEnvVars("./")
-	initializers.ConnectToPostgresDB()
+    initializers.LoadEnvVars("./")
+    initializers.ConnectToPostgresDB()
 }
 
 func main() {
-	engine := handlebars.New("./views", ".hbs")
-	app := fiber.New(fiber.Config{
-		ErrorHandler: initializers.CustomErrorHandler,
-		Views:        engine,
-	})
-	app.Use(cors.New())
-	app.Use(recover.New())
-	app.Use(limiter.New(limiter.Config{
-		Next: func(c *fiber.Ctx) bool {
-			return strings.Contains(c.Path(), "/swagger") // don't limit swagger
-		},
-		Expiration: 10 * time.Second,
-		Max:        300,
-	}))
-	app.Use(logger.New())
-	// TODO: Add authentication middleware
+    engine := handlebars.New("./views", ".hbs")
+    app := fiber.New(fiber.Config{
+        ErrorHandler: initializers.CustomErrorHandler,
+        Views:        engine,
+    })
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowHeaders:  "Origin, Content-Type, Accept",
+    }))
+    app.Use(recover.New())
+    app.Use(limiter.New(limiter.Config{
+        Next: func(c *fiber.Ctx) bool {
+            return strings.Contains(c.Path(), "/swagger") // don't limit swagger
+        },
+        Expiration: 10 * time.Second,
+        Max:        300,
+    }))
+    app.Use(logger.New())
+    // TODO: Add authentication middleware
 
-	app.Static("/", "./public")
-	app.Static("/purecss", "./node_modules/purecss/build")
+    app.Static("/", "./public")
+    app.Static("/purecss", "./node_modules/purecss/build")
 
-	app.Get("/", controllers.Index)
-	app.Get("/articles", controllers.GetAllArticles)
-	app.Get("/articles/:id", controllers.GetArticle)
-	app.Post("/articles", controllers.CreateArticle)
-	app.Patch("/articles/:id", controllers.UpdateArticle)
-	app.Delete("/articles/:id", controllers.DeleteArticle)
+    app.Get("/", controllers.Index)
+    app.Get("/articles", controllers.GetAllArticles)
+    app.Get("/articles/:id", controllers.GetArticle)
+    app.Post("/articles", controllers.CreateArticle)
+    app.Patch("/articles/:id", controllers.UpdateArticle)
+    app.Delete("/articles/:id", controllers.DeleteArticle)
 
-	app.Get("/swagger/*", swagger.HandlerDefault)
+    app.Get("/swagger/*", swagger.HandlerDefault)
 
-	fmt.Println("Server is running on port 3000")
-	port := os.Getenv("PORT")
-	log.Fatal(app.Listen(port)) // listen and serve on 3000
+    fmt.Println("Server is running on port 3000")
+    port := os.Getenv("PORT")
+    log.Fatal(app.Listen(port)) // listen and serve on 3000
 }
